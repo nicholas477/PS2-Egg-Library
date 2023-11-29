aeafe81a5e4a574693f9e1f321166d2e2f2a2375
EPICGameGuy <admin@cactus.vg>
2023-11-29 18:02:29 -0500

    Fixed build.cs

--- PS2LevelEditingToolsLibrary.Build.cs
+++ PS2LevelEditingToolsLibrary.Build.cs
@@ -9,24 +9,12 @@ public class PS2LevelEditingToolsLibrary : ModuleRules
 	{
 		CppStandard = CppStandardVersion.Cpp20;
 		Type = ModuleType.External;
-		PublicSystemIncludePaths.Add("$(ModuleDir)/PS2-Egg-Library/include");
+		PublicSystemIncludePaths.Add("$(ModuleDir)/include");
 
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
 			// Add the import library
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "PS2-Egg-Library", "build", "Debug", "ps2-egg.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "build", "Debug", "ps2-egg.lib"));
 		}
-		// else if (Target.Platform == UnrealTargetPlatform.Mac)
-		// {
-			// PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libExampleLibrary.dylib"));
-			// RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/PS2LevelEditingToolsLibrary/Mac/Release/libExampleLibrary.dylib");
-		// }
-		// else if (Target.Platform == UnrealTargetPlatform.Linux)
-		// {
-			// string ExampleSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", "PS2LevelEditingToolsLibrary", "Linux", "x86_64-unknown-linux-gnu", "libExampleLibrary.so");
-			// PublicAdditionalLibraries.Add(ExampleSoPath);
-			// PublicDelayLoadDLLs.Add(ExampleSoPath);
-			// RuntimeDependencies.Add(ExampleSoPath);
-		// }
 	}
 }
